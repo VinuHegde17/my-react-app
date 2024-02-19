3d2a263550846926481b856071fbca6035eb60ac
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-19 11:26:04 +0530

    Update index.js

--- basic/src/index.js
+++ basic/src/index.js
@@ -2,10 +2,8 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, excepturi?
-    </p>
+    <h1>My favourite food</h1>
+    
   </div>,
   document.getElementById("root")
 );
