4729b4bde5777646fc52000ff9e11bb0950ae2a8
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-19 18:29:45 +0530

    .

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+var React = require("react");
+var ReactDOM = require("react-dom");
+const name="Akash";
+const place="Kundapur";
+const cpy=2019
+ReactDOM.render(
+   <div><div>
+   <h1>Hello World!</h1>
+   <p>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, excepturi?
+   </p>
+ </div>
+ <hr></hr>
+<div>
+  <h1>My favourite food</h1>
+  <ul>
+  <li>Bacon</li>
+  <li>Jamon</li>
+  <li>Noodles</li>
+  </ul>
+</div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+<hr></hr>
+<div>
+<h1>My name is {name+" "+place}</h1>
+<p>My favourite number is {Math.random()}</p>
+</div>
+<hr></hr>
+<div>
+<p>My name is {name}</p>
+<p>©{cpy}</p>
+</div>
+<hr></hr>
+</div>,
+  document.getElementById("root")
+);
 
-export default App;
