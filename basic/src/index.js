4e8891a7d1003f1c7f114ba05323259fb0604935
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-19 12:28:56 +0530

    Update index.js

--- basic/src/index.js
+++ basic/src/index.js
@@ -2,14 +2,15 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 const name="Akash";
 const place="Kundapur";
-
+const cpy=2019
 ReactDOM.render(
    <div><div>
    <h1>Hello World!</h1>
    <p>
      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, excepturi?
    </p>
- </div>,
+ </div>
+ <hr></hr>
 <div>
   <h1>My favourite food</h1>
   <ul>
@@ -17,11 +18,18 @@ ReactDOM.render(
   <li>Jamon</li>
   <li>Noodles</li>
   </ul>
-</div>,
+</div>
+<hr></hr>
 <div>
 <h1>My name is {name+" "+place}</h1>
 <p>My favourite number is {Math.random()}</p>
-</div>,
+</div>
+<hr></hr>
+<div>
+<p>My name is {name}</p>
+<p>©{cpy}</p>
+</div>
+<hr></hr>
 </div>,
   document.getElementById("root")
 );
