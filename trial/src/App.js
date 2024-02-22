056d1b406415ea96c9576886fa21e2482d4e12e7
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-22 15:40:23 +0530

    .

--- trial/src/App.js
+++ trial/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import MyButton from './screens/Btn';
 import About from './screens/About';
 import Contact from './screens/Contact';
+import Title from './screens/Title';
+import Profile from './screens/img';
+import ShoppingList from './screens/Shopping';
 
 function App() {
-  return <div>
-    <About/>
-    <Contact/>
-  </div>
+  return (
+<>
+<MyButton/>
+<About/>
+<Contact/>
+<Title/>
+<Profile/>
+<ShoppingList/>      
+</>
+  );
 }
 
 export default App;
